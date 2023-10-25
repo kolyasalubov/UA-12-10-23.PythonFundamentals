aeb8a791a447c64930572533cc00efe936ea2176
Leonid Chelakhov <leonidchelahov@gmail.com>
2023-10-25 18:18:16 +0300

    chealkhovl

---
+++ Chelakhovl/HW3/3.py
@@ -0,0 +1,10 @@
+#створюємо дві зміни в яких значення буде число
+
+number1 = 10
+number2 = 20
+#за доромогою математичних дій змінив значення змінних
+number1 = number1 + number2
+number2 = number1 - number2
+number1 = number1 - number2
+
+print("number1:", number1, "number2:", number2)
