a4112a29e27676397912ab21bae28a1253c67b05
annvish <an.vishnyakova@gmail.com>
2023-10-25 19:10:13 +0300

    HW3_ 3_1

--- VHanna/HW3/2.py
+++ VHanna/HW3/2.py
@@ -1,1 +1,18 @@
-2
+#add number
+a = input('Your four-digit natural number:')
+
+#1 the product of the digits of this number
+#I split the entered number into 4 separate numbers and convert them into a numeric format
+one = int(a[0])
+two = int(a[1])
+tree = int(a[2])
+four = int(a[3])
+
+#I calculate the product of the four obtained numbers
+product = one*two*tree*four
+
+print(f'the product of the digits of {a}:',product)
+
+#2
+
+#3
