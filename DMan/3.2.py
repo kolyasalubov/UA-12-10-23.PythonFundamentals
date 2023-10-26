a82414fa6042d43aa26d81d6b9b057fb5131f67b
DMan113 <rybencevdmitrij@gmail.com>
2023-10-26 23:07:00 +0300

    completed second task from HW3

---
+++ DMan/3.2.py
@@ -0,0 +1,8 @@
+user_number = input("Write please a four-digit number ")
+digit1 = user_number[0]
+digit2 = user_number[1]
+digit3 = user_number[2]
+digit4 = user_number[3]  
+
+mutiplying_numbers = int(digit1)*int(digit2)*int(digit3)*int(digit4)
+print("Product of the the digits of this number is: ", mutiplying_numbers)
