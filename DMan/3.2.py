df7850c6dd10158745e85eeebb86b3745817765a
DMan113 <rybencevdmitrij@gmail.com>
2023-10-26 23:33:34 +0300

    full completed second task from HW3

--- DMan/3.2.py
+++ DMan/3.2.py
@@ -5,4 +5,9 @@ digit3 = user_number[2]
 digit4 = user_number[3]  
 
 mutiplying_numbers = int(digit1)*int(digit2)*int(digit3)*int(digit4)
-print("Product of the the digits of this number is: ", mutiplying_numbers)
+
+print("Product of the the digits of this number is:", mutiplying_numbers)
+print("Revers order of this number is:", user_number[::-1])
+#print("revers order of your number is:", digit4, digit3, digit2, digit1)
+print("Sorted number is:", sorted(user_number))
+
