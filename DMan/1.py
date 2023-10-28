67be1ccbebe1be7b62499c2f726e1eb0ab944a4f
DMan113 <rybencevdmitrij@gmail.com>
2023-10-28 21:55:43 +0300

    removed HW3 tasks

--- DMan/1.py
+++
@@ -1,7 +0,0 @@
-name = input("Please, write your name:   ")
-age = input("How old are you?  ")
-city = input("Where are you live?  ")
-
-print("Hello", name)
-print("Your age is", age)
-print("You live in", city)
