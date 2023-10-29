8024493450b7bfc90b0046066e00a2fb50237ec3
Danylo Kozii <Angribrin2005@gmail.com>
2023-10-29 15:13:16 +0200

    task 2 is finished

--- DKozii/task2.py
+++ DKozii/task2.py
@@ -1,3 +1,8 @@
 number = input("input youre number")
+a = int(number[-4])
+b = int(number[-3])
+c = int(number[-2])
+d = int(number[-1])
+first = a * b * c * d
 second = number[::-1]
-print(second)
+print(first, second)
