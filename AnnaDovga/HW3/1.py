4d15a21d2ec801717899497544d2b9460f8d8c62
Anna Dovga <anna01.mouse@gmail.com>
2023-10-29 22:37:34 +0200

    creat HW3

---
+++ AnnaDovga/HW3/1.py
@@ -0,0 +1,35 @@
+phylosophy = """1.Beautiful is better than ugly.
+2.Explicit is better than implicit.
+3.Simple is better than complex.
+4.Complex is better than complicated.
+5.Flat is better than nested.
+6.Sparse is better than dense.
+7.Readability counts.
+8.Special cases aren't special enough to break the rules.
+9.Although practicality beats purity.
+10.Errors should never pass silently.
+11.Unless explicitly silenced.
+12.In the face of ambiguity, refuse the temptation to guess.
+13.There should be one-- and preferably only one --obvious way to do it.
+14.Although that way may not be obvious at first unless you're Dutch.
+15.Now is better than never.
+16.Although never is often better than *right* now.
+17.If the implementation is hard to explain, it's a bad idea.
+18.If the implementation is easy to explain, it may be a good idea.
+19.Namespaces are one honking great idea -- let's do more of those!
+20.Beautiful is better than ugly.
+21.Explicit is better than implicit.
+22.Simple is better than complex.
+23.Complex is better than complicated.
+24.Flat is better than nested.
+25.Sparse is better than dense.
+26.Readability counts."""
+
+print('The number of the words better ',phylosophy.count('better'))
+print('The number of the word never ',phylosophy.count('never'))
+print('The number of the word is ', phylosophy.count('is'))
+print()
+
+print(phylosophy.upper())
+print()
+print(phylosophy.replace('i', '&'))
---
+++ AnnaDovga/HW3/3.py
@@ -0,0 +1,4 @@
+first_digit, second_digit = 6, 10
+print(first_digit, '  ', second_digit)
+first_digit, second_digit = second_digit, first_digit
+print(first_digit, '  ', second_digit)
