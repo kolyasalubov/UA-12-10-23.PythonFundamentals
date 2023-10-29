8d012e315f45d24f5c93603709e790579835cfab
Anna Dovga <anna01.mouse@gmail.com>
2023-10-30 01:13:49 +0200

    Created HW4

---
+++ AnnaDovga/HW4/1.py
@@ -0,0 +1,6 @@
+print('Enter the temperature in Celsius ', end = '')
+temper_C = float(input())
+if temper_C < -273.15:
+    print('Error: Temperature below absolute zero')
+else:
+    print('Temperature in Fahrenheit ', temper_C * (9 / 5) + 32)
