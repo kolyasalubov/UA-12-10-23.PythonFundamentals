bf38f4046aa00d2fb71f7fe71d9655daf0b574db
Anna Dovga <anna01.mouse@gmail.com>
2023-10-30 02:28:37 +0200

    created HW5

---
+++ AnnaDovga/HW5/2.py
@@ -0,0 +1,7 @@
+print('Enter n ', end = '')
+n = int(input())
+list_Fibonacci = [0, 1]
+for i in range(1, n - 1):
+    list_Fibonacci.append(list_Fibonacci[i - 1] + list_Fibonacci[i])
+
+print('Sequence of Fibonacci numbers: ', *list_Fibonacci)
