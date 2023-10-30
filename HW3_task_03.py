7740f8648032093be2ae561b2d7809fd51bf2589
Andriy Rybak <andriy.lvov@ukr.net>
2023-10-30 14:55:59 +0200

    delete another files

--- HW3_task_03.py
+++
@@ -1,8 +0,0 @@
-var_1 = "Hello"
-var_2 = "Goodbye"
-
-print(f"Before interchanging: {var_1}, {var_2}")
-
-var_2, var_1 = var_1, var_2
-
-print(f"After interchanging: {var_1}, {var_2}")
--- LitvischenkoYevhen/HW3/HW3_task_3.py
+++
@@ -1,9 +0,0 @@
-value_1 = input('Enter value_1 : ')
-value_2 = input('Enter value_2 : ')
-print('\n Before interchange :')
-print(f'value_1 :{value_1}   ID :{id(value_1)}')
-print(f'value_2 :{value_2}   ID :{id(value_2)}')
-value_1, value_2 = value_2, value_1
-print('\n After interchange :')
-print(f'value_1 :{value_1}   ID :{id(value_1)}')
-print(f'value_2 :{value_2}   ID :{id(value_2)}')
