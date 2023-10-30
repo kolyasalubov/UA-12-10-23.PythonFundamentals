b7bebad4ab7adab8c1fca0a7fb49c09db4ab4b15
Andriy Rybak <andriy.lvov@ukr.net>
2023-10-30 15:35:40 +0200

    del

--- IvanKorzh/HW3/2.py
+++
@@ -1,6 +0,0 @@
-number = int(input("Enter 4-digit natural number:"))
-st_number = str(number)
-count = 1
-for element in st_number:
-    count *= int(element)
-print(f"It is product of the digits of number: {count}")
--- ShtefanIlya/HW3/hw_3.3.py
+++
@@ -1,9 +0,0 @@
-var_1 = "a"
-var_2 = "b"
-print(var_1)
-print(var_2)
-var_1, var_2 = var_2, var_1
-print(var_1)
-print(var_2)
-
-
