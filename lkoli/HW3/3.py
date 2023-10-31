478e466645ce74980409a2a9a80779b9352e17cd
Liubov Koliasa <kolyasa.lubov@gmail.com>
2023-10-31 19:09:23 +0200

    update

--- lkoli/HW3/3.py
+++ lkoli/HW3/3.py
@@ -1,1 +1,9 @@
-print("task3")
+weather = "raining"
+
+# if weather == "raining":
+#     print("Open Your umbrella")
+# else:
+#     print("Wear your cap")
+
+
+print("Open Your umbrella" if weather == "raining" else "Wear your cap")
