52719b6eeeef29ebdf2e710b153d46d6463609cf
Evhen Glotov <evegney.glotov@gmail.com>
2023-11-05 16:19:59 +0200

    Added task_1.py and task_2.py for HW6

---
+++ GlotovES/HW6/task_2.py
@@ -0,0 +1,8 @@
+while True:
+    login = input("Enter your login: ")
+    
+    if login == "First":
+        print("Welcome, First!")
+        break
+    else:
+        print("Error: Invalid login. Try again.")
