60bf3225368946469df34122e17395b02bd915e0
Lyvyshchenko Yevhen <litvischenkoev@gmail.com>
2023-11-06 11:58:31 +0200

    HW7.1 completed

--- LitvischenkoYevhen/HW7/HW7_task_1.py
+++
@@ -1,8 +0,0 @@
-def largest_number(number_1, number_2):
-    if number_1 < number_2:
-        return number_2
-    elif number_1 > number_2:
-        return number_1
-    else:
-        print("numbers are equal")
-        return None
