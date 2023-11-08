4ebd92c8b618d2b07dab7f49497cb89e3b81e22e
annvish <an.vishnyakova@gmail.com>
2023-11-08 18:47:07 +0300

    done HW 7 task 2

--- VHanna/HW7/HW7_task2.py
+++ VHanna/HW7/HW7_task2.py
@@ -35,3 +35,70 @@ def number_to_string(num):
 def reverse(st):
     s = st.split()
     return ' '.join(s[::-1])
+
+#6 Reverse List Order
+#In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+def reverse_list(l):
+    return l[::-1]
+
+#7 Multiples of 3 or 5
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+#Additionally, if the number is negative, return 0.
+#Note: If the number is a multiple of both 3 and 5, only count it once.
+
+def solution(number):
+    a_3=[]
+    if number <= 0: return 0
+    else:
+        for i in range(1,number):
+            if i%3 == 0 or i%5 == 0:
+                a_3.append(i)
+        return sum(a_3)
+    
+#8 Will you make it?
+#You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+#Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+#Function should return true if it is possible and false if not.
+
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    if distance_to_pump <= mpg*fuel_left:
+        return True
+    else: return False
+
+#9 Are You Playing Banjo?
+#Create a function which answers the question "Are you playing banjo?".
+#If your name starts with the letter "R" or lower case "r", you are playing banjo!
+#The function takes a name as its only argument, and returns one of the following strings:
+#name + " plays banjo" 
+#name + " does not play banjo"
+#Names given are always valid strings.
+
+def are_you_playing_banjo(name):
+    if name[0] == 'R' or name[0] == 'r':
+        return name + " plays banjo" 
+    else: return name + " does not play banjo" 
+
+#10 Convert boolean values to strings 'Yes' or 'No'.
+#Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+def bool_to_word(boolean):
+    if boolean: return 'Yes'
+    else: return "No"
+
+#11 Counting sheep...
+#Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+def count_sheeps(sheep):
+    return sheep.count(True)
+
+#12 Is this my tail?
+#Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+#If the tail is right return true, else return false.
+#The arguments will always be non empty strings, and normal letters.
+
+def correct_tail(body, tail):
+    if body[-1] == tail:
+        return True
+    return False
