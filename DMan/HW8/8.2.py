6d225703abaf62ae7857934d6380b9548b8327c6
DMan113 <rybencevdmitrij@gmail.com>
2023-11-11 20:23:59 +0200

    done task8.2

---
+++ DMan/HW8/8.2.py
@@ -0,0 +1,18 @@
+import re
+
+def validity_of_password():
+    p= input("Input your password  ")
+
+    while True:  
+        if (len(p)<6 or len(p)>12):
+            return "Invalid Password"
+        elif not re.search("[a-z]",p):
+            return "Invalid Password"
+        elif not re.search("[0-9]",p):
+            return "Invalid Password"
+        elif not re.search("[A-Z]",p):
+            return "Invalid Password"
+        elif not re.search("[$#@]",p):
+            return "Invalid Password"
+        else:
+            return "Valid Password"
--- DMan/HW8/8.3/user_module.py
+++ DMan/HW8/8.3/user_module.py
@@ -18,4 +18,3 @@ while True:
             break
         case _:
             print("invalid input")
-
