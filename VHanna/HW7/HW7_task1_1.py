4601f174dbde4383de7dcee1ef6d07f65374c47d
annvish <an.vishnyakova@gmail.com>
2023-11-12 22:53:12 +0300

    HW7 task 1_1

---
+++ VHanna/HW7/HW7_task1_1.py
@@ -0,0 +1,7 @@
+def max_number(x1, x2):
+    return max(x1,x2)
+
+number_1 = input('Enter number 1: ')
+number_2 = input('Enter number 2: ')
+find_max = max_number(number_1,number_2)
+print(find_max)
