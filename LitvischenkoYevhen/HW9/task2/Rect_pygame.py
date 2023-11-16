84d7545df4243af121ac6c95d5a81d6cdc1cdccc
Litvischenko Yebven <litvischenkoev@gmail.com>
2023-11-16 17:24:35 +0200

    HW9 is done/ fix resizeble window

--- LitvischenkoYevhen/HW9/task2/Rect_pygame.py
+++ LitvischenkoYevhen/HW9/task2/Rect_pygame.py
@@ -2,8 +2,8 @@ import pygame
 
 FPS = 60
 
-WIDTH_DISPLAY=500
-HEIGHT_DISPLAY=500
+width=200
+height=200
 
 COORD_X=50
 COORD_Y=50
@@ -17,7 +17,7 @@ RED_COLOR = (250, 0, 0)
 pygame.init()
 
 
-gameDisplay = pygame.display.set_mode((WIDTH_DISPLAY, HEIGHT_DISPLAY), pygame.RESIZABLE)
+gameDisplay = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 
 pygame.display.set_caption("My first game")
 
@@ -27,20 +27,22 @@ clock = pygame.time.Clock()
 
 while run:
     pygame.time.delay(100)
-    
+        
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == pygame.VIDEORESIZE: # if resize window
+            width, height = event.w, event.h
 
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_LEFT] and (COORD_X >= DELTA_STEP) :
         COORD_X = COORD_X-DELTA_STEP
-    if keys[pygame.K_RIGHT] and (COORD_X < WIDTH_DISPLAY - WIDTH_RECTANGLE):
+    if keys[pygame.K_RIGHT] and (COORD_X < width - WIDTH_RECTANGLE):
         COORD_X = COORD_X+DELTA_STEP
     if keys[pygame.K_UP] and (COORD_Y >= DELTA_STEP):
         COORD_Y = COORD_Y-DELTA_STEP
-    if keys[pygame.K_DOWN] and (COORD_Y < HEIGHT_DISPLAY - HEIGHT_RECTANGLE):
+    if keys[pygame.K_DOWN] and (COORD_Y < height - HEIGHT_RECTANGLE):
         COORD_Y = COORD_Y+DELTA_STEP
 
 
