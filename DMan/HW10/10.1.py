35080c049d89dedd09d3b8860d0d334fc16cf111
DMan113 <rybencevdmitrij@gmail.com>
2023-11-18 21:52:29 +0200

    solved first part from 10.1 task

---
+++ DMan/HW10/10.1.py
@@ -0,0 +1,2 @@
+def kb():
+    pass
---
+++ DMan/HW10/10.1/10.1.1.py
@@ -0,0 +1,12 @@
+class Poligon:
+    pass
+
+
+class Rectangle(Poligon):
+    weight: float
+    height: float
+    
+    
+    def rect_sqr():
+        return "Square of rectangle is: ", Rectangle.weight * Rectangle.height
+
