7a4681b2afcbc95094aef7b245c7e45a321752fa
DMan113 <rybencevdmitrij@gmail.com>
2023-11-19 22:41:49 +0200

    created Task 10.1.2

--- DMan/HW10/10.1/10.1.1.py
+++ DMan/HW10/10.1/10.1.1.py
@@ -11,6 +11,7 @@ class Polygon:
         for i in range(self.num_sides):
             print(f"Side {i+1} is {self.sides[i]}")
 
+
 class Rectangle(Polygon):
     def __init__(self):
         super().__init__(2)
@@ -18,51 +19,9 @@ class Rectangle(Polygon):
     def find_area(self):
         area = self.sides[0] * self.sides[1]
         print("area of rectangle is ", area)
-    
+
+
 r = Rectangle()
-# r.input_sides()
-# r.display_sides()
+r.input_sides()
+r.display_sides()
 r.find_area()
-
-# class Poligon:
-#     def __init__(self, num_of_sides):
-#         self.n = num_of_sides
-#         self.sides = [0 for i in range(num_of_sides)]
-
-#     def inputSides(self):
-#         self.sides = [float(input("Enter side " + str(i + 1) +" : "))
-#                       for i in range(self.n)]
-
-#     def dispSides(self):
-#         for i in range(self.n):
-#             print("Side", i+1, "is", self.sides[i])
-
-
-
-# class Poligon:
-#     def __init__(self, num_of_side):
-#         self.num_of_side = num_of_side
-#         self.sides = [0 for i in range(num_of_side)]
-
-#     def findArea(self):
-#        for i in range(self.num_of_side):
-#             self.sides.append(0)
-
-#     def inputSides(self):
-#         for i in range(self.num_of_side):
-#             user_input = float(input("Enter your side"))
-#             self.sides[i] = user_input
-
-#     def disprides (self):
-#         print(self.sides)
-
-# class Rectangle(Poligon):
-#     def __init__(self):
-#         super().__init__(2)
-
-#     def findArea(self):
-#         if len(self.sides) == self.num_of_side: 
-#           a, b = self.sides
-#           print('The area of the rectangle is %0.2f' % (a * b))
-
-# print(Rectangle.findArea(3))   
--- DMan/HW10/10.1/10.1.2.py
+++ DMan/HW10/10.1/10.1.2.py
@@ -1,2 +1,35 @@
+import random
+
+
+messages = ["Nice day to learn a something new!",
+            "Blablabla", 
+            "Two Jews once meet and...",
+            "Hey, Boss! Where is my salary?"]
+random_message = print(random.choice(messages))
+
+
 class Human:
-    name = str
+    species = "Homosapiens"  
+
+    def __init__(self, name):
+        self.name = name  
+
+    def greeting(self):
+        return print(f"Hello, {self.name}! Welcome!")
+
+    @classmethod
+    def get_species(cls):
+        return print(cls.species)
+
+    @staticmethod
+    def get_random_message():
+        return random_message
+    
+
+person1 = Human("Liubov Koliasa")
+person2 = Human("Dmytro")
+
+h = Human
+person1.greeting()
+person1.get_species()
+h.get_random_message()
