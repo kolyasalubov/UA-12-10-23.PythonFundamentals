586378e8b558896d8bde51a3cfaa48264d206b9c
DMan113 <rybencevdmitrij@gmail.com>
2023-11-19 23:38:57 +0200

    update

--- DMan/HW10/10.1/10.1.3.py
+++ DMan/HW10/10.1/10.1.3.py
@@ -9,11 +9,13 @@ class Employee:
         self.salary = salary
         Employee.employee_count += 1
 
-    def displey_employee(self):
+    def displey_employee_VS_salary(self):
         print(f"Name: {self.name}, Salary is: {self.salary}$")
 
     def displey_employee_count(cls):
-        print(f"Total emloyees: {cls.employee_count}")
+        print(f"Total emloyees: {Employee.employee_count}")
+        # print(f"Total emloyees: {cls.employee_count}")
+        
 
 
 print(Employee.__base__)
