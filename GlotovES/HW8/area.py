30e5a7b0833047d21806a097a6e7ac8a850681ec
Evhen Glotov <evegney.glotov@gmail.com>
2023-11-22 21:47:23 +0200

    Added task_2.py for HW11

--- GlotovES/HW8/area.py
+++
@@ -1,9 +0,0 @@
-# def cra(length, width):
-#     '''
-#     This function calculate rectangle area
-#     '''
-#     rectangle_area = length * width
-#     return rectangle_area
-import sys
-
-sys.path
