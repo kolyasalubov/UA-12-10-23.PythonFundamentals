06c368a2304f905d8b1ea9208ef31fe87dcb9ef5
Ivan Likanov <ivan01082005@gmail.com>
2023-12-10 14:02:51 +0200

    Added Final Project

---
+++ LikanovIvan/FinalProject/words.py
@@ -0,0 +1,22 @@
+names = {'computer'}
+
+data_set = {
+    # weather
+    'what\'s weather' : 'weather give me a second',
+    'what\'s outside' : 'weather give me a second',
+
+    #passive
+    'hello': 'passive Hello, I am waiting for commands',
+    "How are you" : 'passive good as always',
+    "what's up" : 'passive good as always',
+
+    #browser
+    'open browser' : 'browser yes, sir',
+    'start google' : 'browser of course',
+    
+    #off
+    'turn of' : 'off_bot Okay',
+
+    #game
+    'start my game' : 'my_game Yes, sir'
+}
